5abd912dc165c3cbbf6ea1fe590aa0476a899334
sammy144 <samreeves1404@gmail.com>
2025-02-20 14:37:52 +0530

    next js portfolio built

--- src/app/components/BlogButton.js
+++ src/app/components/BlogButton.js
@@ -1,13 +1,13 @@
 'use client'
 
 import React from 'react';
-import { Link } from 'react-router-dom'; // Make sure you have react-router-dom installed
+import Link from 'next/link'; // Updated import for Next.js
 import "../components/BlogButton.css"; // You'll need to create this CSS file
 
 const BlogButton = () => {
   return (
     <div id="blog">
-      <Link to="/blog">
+      <Link href="/blog"> {/* Updated to use Next.js Link */}
         <div className="blogButton">
           <svg className="left-icon" width="15px" height="15px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17.093,1.293l-11.2,11.2a.99.99,0,0,0-.242.391l-1.6,4.8A1,1,0,0,0,5,19a1.014,1.014,0,0,0,.316-.051l4.8-1.6a1.006,1.006,0,0,0,.391-.242l11.2-11.2a1,1,0,0,0,0-1.414l-3.2-3.2A1,1,0,0,0,17.093,1.293ZM9.26,15.526l-2.679.893.893-2.679L17.8,3.414,19.586,5.2ZM3,21H20a1,1,0,0,1,0,2H3a1,1,0,0,1,0-2Z"/></svg>
           <h5 className='blogButtonText'>Visit my Blog!</h5>  
